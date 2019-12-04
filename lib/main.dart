32a1fece675e0ad8f0470f2c5c30aa4ed1b56030
Carlos Tiago <ontiago@gmail.com>
2019-12-04 19:42:29 -0300

    Create map list

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
   List _toDoList = [];
+  void addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +43,7 @@ class _HomeState extends State<Home> {
                 children: <Widget>[
                   Expanded(
                       child: TextField(
+                          controller: _toDoController,
                           decoration: InputDecoration(
                               labelText: "Nova Tarefa",
                               labelStyle:
@@ -41,10 +52,31 @@ class _HomeState extends State<Home> {
                       color: Colors.blueAccent,
                       child: Text("Add"),
                       textColor: Colors.white,
-                      onPressed: () {})
+                      onPressed: addToDo)
                 ],
               ),
-            )
+            ),
+            Expanded(
+                child: ListView.builder(
+              padding: EdgeInsets.only(top: 10.0),
+              itemCount: _toDoList.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  title: Text(_toDoList[index]["title"]),
+                  value: _toDoList[index]["ok"],
+                  secondary: CircleAvatar(
+                    child: Icon(
+                        _toDoList[index]["ok"] ? Icons.check : Icons.error),
+                  ),
+                  onChanged: (c) {
+                    setState(() {
+                      _toDoList[index]["ok"] = c;
+                    });
+                    print(c);
+                  },
+                );
+              },
+            ))
           ],
         ));
   }
